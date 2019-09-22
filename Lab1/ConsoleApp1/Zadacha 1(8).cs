43379fb35de5f7f8c2323101463e54ba458dfb7c
LenaKyrychenko <55666415+LenaKyrychenko@users.noreply.github.com>
2019-09-22 21:11:20 +0300

    Add files via upload

---
+++ Lab1/ConsoleApp1/Zadacha 1(8).cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b,c,ser;
+
+            Console.WriteLine("Enter first number: ");
+            a= Convert.ToDouble (Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            b =Convert.ToDouble (Console.ReadLine());
+
+            Console.WriteLine("Enter third number: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            ser =Convert.ToDouble((a + b + c) / 3);
+
+            Console.WriteLine("Ser=" + ser);
+            
+        }
+    }
+}
