9b270027fbdd5bb31cca54dc02d5edc52c9c81b0
LenaKyrychenko <55666415+LenaKyrychenko@users.noreply.github.com>
2019-09-22 22:48:09 +0300

    Add files via upload

---
+++ Lab 2/ConsoleApp10/Zadacha 10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = Convert.ToInt32(Console.ReadLine());
+            int r = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] - arr[j] == n)
+                    {
+                        r++;
+                    }
+                }
+            }
+            Console.WriteLine("Pairs by Difference: " + r);
+        }
+    }
+}
---
+++ Lab 2/ConsoleApp11/Zadacha 11.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array: ");
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum1 = 0, sum2 = 0;
+            int index = 0;
+            bool b = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum1 = 0;
+                sum2 = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    sum1 += arr[j];
+                }
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    sum2 += arr[j];
+                }
+                if (sum1 == sum2)
+                {
+                    index = i;
+                    b = true;
+                    break;
+                }
+            }
+
+            if (b == true)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
