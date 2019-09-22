7adcc3d31ea99151eaca272613e82090566720db
LenaKyrychenko <55666415+LenaKyrychenko@users.noreply.github.com>
2019-09-22 21:12:33 +0300

    Add files via upload

---
+++ Lab1/ConsoleApp2/Zadacha 2(9).cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, h, S;
+
+            Console.WriteLine("Enter first side: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second side: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the hide: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            S = Convert.ToDouble(((a + b) / 2) * h);
+
+            Console.WriteLine("Area:" + S);
+        }
+    }
+}
