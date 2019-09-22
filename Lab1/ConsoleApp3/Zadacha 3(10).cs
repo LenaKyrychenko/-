8887c70eb6f8ddad301a0f445f4491b32d229404
LenaKyrychenko <55666415+LenaKyrychenko@users.noreply.github.com>
2019-09-22 21:13:52 +0300

    Add files via upload

---
+++ Lab1/ConsoleApp3/Zadacha 3(10).cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, ld;
+
+            Console.WriteLine("Enter number:");
+            n = Convert.ToDouble(Console.ReadLine());
+
+            ld = Convert.ToDouble(n % 10);
+
+            Console.WriteLine("Last Digit is " + ld);
+        }
+    }
+}
---
+++ Lab1/ConsoleApp4/Zadacha 4(11).cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num, n, k; 
+            double nDig;
+
+            Console.WriteLine("Enter number:");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter n:");
+            n = Convert.ToDouble(Console.ReadLine());
+
+            k = Math.Pow(10, n - 1);
+
+            nDig = ((num / k) % 10);
+            nDig = Math.Floor(nDig);
+
+            
+                Console.WriteLine("nDigit is  " + nDig);
+            
+        }
+    }
+}
