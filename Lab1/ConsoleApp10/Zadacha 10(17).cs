cb90b32ab456bca866ba2cc951cdb71c9e9c2f18
LenaKyrychenko <55666415+LenaKyrychenko@users.noreply.github.com>
2019-09-22 21:18:54 +0300

    Add files via upload

---
+++ Lab1/ConsoleApp10/Zadacha 10(17).cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, fact=1;
+
+            Console.WriteLine("Enter n:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= n; i++)
+                fact = fact * i;
+
+            Console.WriteLine("Factorial: " + fact);
+
+        }
+    }
+}
---
+++ Lab1/ConsoleApp8/Zadacha 8(15).cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            bool product=true;
+
+            Console.WriteLine("Enter a:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter b:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter c:");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            if (((a < 0) && (b > 0) && (c > 0)) || ((a > 0) && (b < 0) && (c > 0)) || ((a > 0) && (b > 0) && (c < 0)))
+                product = false;
+            if ((a < 0) && (b < 0) && (c < 0))
+                product = false;
+            if ((a > 0) && (b > 0) && (c > 0))
+                product = true;
+            if (((a < 0) && (b < 0) && (c > 0)) || ((a > 0) && (b < 0) && (c < 0)) || ((a < 0) && (b > 0) && (c < 0)))
+                product = true;
+
+            if (product == true)
+                Console.WriteLine("Product is POSITIVE");
+            if (product == false)
+                Console.WriteLine("Product is NEGATIVE");
+        }
+    }
+}
