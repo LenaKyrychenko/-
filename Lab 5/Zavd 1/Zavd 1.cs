3cc7a3fffb663f020b247c25881204d6e172cf17
LenaKyrychenko <55666415+LenaKyrychenko@users.noreply.github.com>
2019-10-08 14:26:54 +0300

    Add files via upload

---
+++ Lab 5/Zavd 1/Zavd 1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab_5
+{
+    class Box
+    {
+        public double l { get; set; }
+        public double w { get; set; }
+        public double h { get; set; }
+        
+        public Box(double L, double W, double H) { l = L; w = W; h = H; }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double l, w, h,sa,lsa,v;
+
+            Console.Write("Length: ");
+            l = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Width: ");
+            w = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Height: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            Box b = new Box(l,w,h);
+
+            sa = (2 * b.l * b.w) + (2 * b.l* b.h) + (2 * b.w * b.h);
+            Console.WriteLine("Surface Area - " + sa);
+
+            lsa = (2 * b.l * b.h) + (2 * b.w * b.h);
+            Console.WriteLine("Lateral Surface Area - " + lsa);
+
+            v = b.l * b.w * b.h;
+            Console.WriteLine("Volume - " + v);
+
+
+        }
+    }
+}
---
+++ Lab 5/Zavd 2/Zavd 2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Zavd_2
+{
+    class Box
+    {
+        public double l { get; set; }
+        public double w { get; set; }
+        public double h { get; set; }
+
+        public Box(double L, double W, double H)
+        {
+            if (L <= 0)
+            {
+                Console.WriteLine("Length cannot be zero or negative");
+                Environment.Exit(0);
+            }
+            else
+            {
+                l = L;
+            }
+
+            if (W <= 0)
+            {
+                Console.WriteLine("Width cannot be zero or negative");
+                Environment.Exit(0);
+            }
+            else
+            {
+                w = W;
+            }
+
+            if (H <= 0)
+            {
+                Console.WriteLine("Height cannot be zero or negative");
+                Environment.Exit(0);
+            }
+            else
+            {
+                h = H;
+            }
+
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l, w, h, sa, lsa, v;
+
+            Console.Write("Length: ");
+            l = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Width: ");
+            w = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Height: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            Box b = new Box(l, w, h);
+
+           
+            sa = (2 * b.l * b.w) + (2 * b.l * b.h) + (2 * b.w * b.h);
+            Console.WriteLine("Surface Area - " + sa);
+
+            lsa = (2 * b.l * b.h) + (2 * b.w * b.h);
+            Console.WriteLine("Lateral Surface Area - " + lsa);
+
+            v = b.l * b.w * b.h;
+            Console.WriteLine("Volume - " + v);
+        }
+    }
+}
